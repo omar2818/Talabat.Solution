6e46bbf6d505084be2b3015ad9acbcaf8ba84bc3
Omar Wael <waelmero28@gmail.com>
2024-04-10 23:52:49 +0200

    Add Product Module Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class Product : BaseEntity
+	{
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public int BrandId { get; set; } // Foreign Key Column
+
+        public ProductBrand Brand { get; set; } // Navigational Property
+
+        public int CategoryId { get; set; }
+
+        public ProductCategory Category { get; set; }
+    }
+}
