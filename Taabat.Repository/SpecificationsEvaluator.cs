7f6cb4299e98b52d84397a70310a26d52a6d2c68
Omar Wael <waelmero28@gmail.com>
2024-04-16 02:21:25 +0200

    Add Specification Design Pattern

---
+++ Taabat.Repository/SpecificationsEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+
+namespace Talabat.Repository
+{
+	internal static class SpecificationsEvaluator<TEntity> where TEntity : BaseEntity
+	{
+		public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecifications<TEntity> spec)
+		{
+			var query = inputQuery;
+
+			if(spec?.Criteria is not null)
+			{
+				query = query.Where(spec.Criteria);
+			}
+			
+			query = spec.Includes.Aggregate(query, (currentQuery, includeExpression) => currentQuery.Include(includeExpression));
+
+			return query;
+		}
+	}
+}
--- Talabat.Core/Repositories.Contract/IGenericRepository.cs
+++ Talabat.Core/Repositories.Contract/IGenericRepository.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
 
 namespace Talabat.Core.Repositories.Contract
 {
 	public interface IGenericRepository<T> where T : BaseEntity
 	{
 		Task<T?> GetAsync(int id);
+
 		Task<IEnumerable<T>> GetAllAsync();
+
+		Task<T?> GetWithSpecAsync(ISpecifications<T> spec);
+		Task<IEnumerable<T>> GetAllWithSpecAsync(ISpecifications<T> spec);
 	}
 }
