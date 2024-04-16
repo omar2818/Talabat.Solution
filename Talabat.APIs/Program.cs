acb1642e8b597eb40c28ea37eb7285fc09c3ba86
Omar Wael <waelmero28@gmail.com>
2024-04-17 01:06:18 +0200

    Handling Not Found Endpoint

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -94,6 +94,8 @@ namespace Talabat.APIs
 				//app.UseDeveloperExceptionPage();
 			}
 
+			app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 			app.UseHttpsRedirection();
 
 			app.UseStaticFiles();
