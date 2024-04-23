e6ef08b774c52025a080fb33b0b78503d20956a2
Omar Wael <waelmero28@gmail.com>
2024-04-23 02:26:29 +0200

    Validate Customer Basket

--- Talabat.APIs/Controllers/BasketController.cs
+++ Talabat.APIs/Controllers/BasketController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.DTOs;
 using Talabat.APIs.Errors;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
@@ -9,10 +11,14 @@ namespace Talabat.APIs.Controllers
 	public class BasketController : BaseAPIController
 	{
 		private readonly IBasketRepository _basketRepository;
+		private readonly IMapper _mapper;
 
-		public BasketController(IBasketRepository basketRepository)
+		public BasketController(
+			IBasketRepository basketRepository,
+			IMapper mapper)
         {
 			_basketRepository = basketRepository;
+			_mapper = mapper;
 		}
 
 		[HttpGet()]
@@ -24,9 +30,10 @@ namespace Talabat.APIs.Controllers
 		}
 
 		[HttpPost]
-		public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+		public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
 		{
-			var createdOrUpdatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+			var mappedBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+			var createdOrUpdatedBasket = await _basketRepository.UpdateBasketAsync(mappedBasket);
 			
 			if(createdOrUpdatedBasket is null)
 			{
